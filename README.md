96063e53fae0faa63d069e4d90fe87e520af71d4
Shon Diaz <shondiaz@gmail.com>
2016-09-17 16:05:40 -0500

    improve the upload script. still need a download script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # aws_s3_benchmarking
-a quick upload and download test using aws s3 and statsD
+a quick upload and download test using aws s3 and statsD  
+
+## usage
+```node ./upload.js```
+
+## note
+- should be used on many other machines at the same time
---
+++ upload.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const path = require('path');
+var mime = require('mime');
+var conf = require('./conf.json');
+var AWS = require('aws-promised');
+var fs = require('fs-extra-promise');
+var stats = require('udp_stats').configure({
+    mode: "udp",
+    host: "ec2-54-165-139-237.compute-1.amazonaws.com",
+    port: 8125,
+    type: "count",
+    metricName: "aws.s3.upload"
+});
+
+var s3 = new AWS.s3();
+
+var droppedConnections = 0;
+
+function main() {
+
+    let startTime = 0;
+    let stopTime = 0;
+
+    console.log("BEGIN");
+
+    new Promise(function(resolve, reject){
+
+        let fileList = [];
+
+        fs.walk(conf.directory)
+            .on('data', function (file) {
+
+                if (new RegExp(conf.fileFilter, 'i').test(file.path))
+                    fileList.push(file.path);
+                else
+                    console.warn(`IGNORE: ${file.path}`);
+
+            })
+            .on('end', function(){resolve(fileList);})
+            .on('error', reject);
+    })
+    .then(function(fileList){
+
+        console.log(`discovered ${fileList.length} files`);
+
+        startTime = stats.start();
+        return groupUpload(fileList, conf.concurrentUploads);
+    }).then(function(){
+
+        stopTime = stats.stop();
+        console.log(`done!\n\n ${startTime}-${stopTime}=${startTime-stopTime} with ${droppedConnections} dropped`);
+
+    }).catch(console.error);
+}
+
+function groupUpload(fileList, groupSize){
+
+    if(!fileList.length)
+        return Promise.resolve();
+
+    return new Promise(function(resolve, reject){
+
+        let returnCount = 0;
+
+        groupSize = Math.min(groupSize, fileList.length);
+
+        fileList.slice(0, groupSize).forEach(function(filePath){
+            let fileName = path.win32.basename(filePath);
+            let body = fs.createReadStream(filePath);
+            body.read();
+
+            var params = {
+                Bucket: conf.bucketName,
+                Key: fileName,
+                ContentType: mime.lookup(fileName),
+                Body: body
+            };
+
+            s3.makeUnauthenticatedRequest('putObject', params, function (err, result) {
+
+                if (err){
+                    droppedConnections++;
+                    console.error(err);
+                } else {
+
+                    stats.send(1).then(function(){
+                        console.log("STATS: +1");
+                    });
+                }
+
+                if(++returnCount === groupSize){
+                    console.log(`${fileList.length-groupSize} left!`);
+                    resolve(groupUpload(fileList.slice(groupSize), groupSize));
+                }
+            });
+        });
+    });
+}
+
+
+main();
